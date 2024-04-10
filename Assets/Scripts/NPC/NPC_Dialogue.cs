ba60eddd988a902a38c3e7b34349927eda50b3bf
Willian Renan Rodrigues <willdesign.will@gmail.com>
2024-04-10 20:39:10 -0300

    Chamando metodo
    - chamando metodo showdialog()

--- Assets/Scripts/NPC/NPC_Dialogue.cs
+++ Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 public class NPC_Dialogue : MonoBehaviour
 {
@@ -14,9 +15,9 @@ public class NPC_Dialogue : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        ShowDialogue();
     }
 
     void ShowDialogue()
